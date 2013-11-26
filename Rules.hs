31e02ea22ef4f3d03d8bea6ddfd11b14574de446
Dambaev Alexander <dambaev@dambaev-host.baikalbank.ru>
2013-11-26 11:01:28 +0900

    added ports parser

--- Rules.hs
+++ Rules.hs
@@ -7,12 +7,12 @@ import Network.AD.SID
 import Control.Monad
 
 session = ProxySession
-    { sessionUserSID = SIDUser "user1"
+    { sessionUserSID = SIDUser "S-1-5-21-1085031214-2146703713-725345543-12922"
     , sessionGroupsSIDs = [ SIDGroup "group1", SIDGroup "group2"]
     , sessionDate = DateYYYYMMDD 2013 11 22
     , sessionTime = TimeHHMM 09 00
     , sessionWeekDay = 4
-    , sessionDestination = DestinationAddrPort (IPAddress "192.168.11.30") 1521
+    , sessionDestination = DestinationAddrPort (IPAddress "192.168.11.30") 2010
     }
 
 main = do
